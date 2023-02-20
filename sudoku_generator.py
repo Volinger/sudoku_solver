6294b30b121b91fb4d9fe72849a21b24074b0c68
Volinger <tisovskyerik@gmail.com>
2023-02-20 23:23:45 +0100

    generating completed sudoku

--- sudoku_generator.py
+++ sudoku_generator.py
@@ -7,8 +7,11 @@ class Sudoku:
     # if no position is available, return to previous generation step
     # repeat for all numbers until all positions filled
     def __init__(self):
-        self.total_size = 4 #size of sudoku = total_size * total_size, can be 2,4,9
+        self.total_size = 9 #size of sudoku = total_size * total_size, can be 2,4,9
         self.grid = np.zeros((self.total_size, self.total_size))
+        self.cell_size = int(self.total_size**0.5)
+        self.possible_numbers = [[list(range(self.total_size))]*self.total_size]*self.total_size
+        self.number_selection_memory = []
 
     def generate_grid(self):
         np.random.seed(50)
@@ -17,24 +20,25 @@ class Sudoku:
 
     def single_number_fill(self, number):
         for row_number, row in enumerate(self.grid):
-            generated_position = [row_number, np.random.randint(1, self.total_size)]
+            choices = np.arange(0, self.total_size)
             rules_passed = False
             out_of_options = False
-            while not (rules_passed or out_of_options):
+            while not rules_passed:
+                generated_position = [row_number, np.random.choice(choices)]
                 rules_passed = self.check_rules(generated_position, number)
                 if rules_passed:
                     self.grid[tuple(generated_position)] = number
                 else:
-                    if generated_position[1] <= (-self.total_size):
-                        out_of_options = True
-                    generated_position[1] -= 1
+                    choices = np.delete(choices, np.where(choices == generated_position[1]))
+                    out_of_options = not choices.any()
             if out_of_options:
-                raise ArithmeticError
+                break
 
     def check_rules(self, position_to_occupy, number):
         column_ok = self.check_column(position_to_occupy[1], number)
         position_ok = self.check_position(position_to_occupy)
-        rules_passed = all([column_ok, position_ok])
+        cell_ok = self.check_cell(position_to_occupy, number)
+        rules_passed = all([column_ok, position_ok, cell_ok])
         return rules_passed
 
     def check_position(self, position_to_occupy):
@@ -43,10 +47,14 @@ class Sudoku:
 
     def check_column(self, column_index, number):
         column = self.grid[:, column_index]
-        return not np.isin(number, column)
+        return not (number in column)
 
-    def check_cell(self, cell, number):
-        pass
+    def check_cell(self, position, number):
+        cell_x = (position[0] // self.cell_size) * self.cell_size
+        cell_y = (position[1] // self.cell_size) * self.cell_size
+        cell = self.grid[cell_x:cell_x + self.cell_size, cell_y:cell_y + self.cell_size]
+        result = (number in cell)
+        return not result
 
 x = Sudoku()
 x.generate_grid()
