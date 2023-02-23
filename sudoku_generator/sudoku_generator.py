7f5c7a2eb4556f7ef3a2db47a675d29c6ca376d3
Erik_Tisovsky <erik.tisovsky@anv-tech.com>
2023-02-23 18:54:35 +0100

    working on rollback functionality

--- sudoku_generator/sudoku_generator.py
+++ sudoku_generator/sudoku_generator.py
@@ -10,9 +10,14 @@ class Sudoku:
         self.possible_numbers.fill(True)
         self.number_selection_memory = []
         self.number_selection_random = []
+        self.rollbacked_filter = np.ndarray((self.total_size, self.total_size, self.total_size), dtype=bool)
+        self.rollbacked_filter.fill(True)
+
+    def get_allowed_numbers(self):
+        return np.multiply(self.possible_numbers, self.rollbacked_filter)
 
     def generate_grid(self):
-        np.random.seed(50)
+        np.random.seed(51)
         while np.any(self.grid == 0):
             self.randomly_fill_next_position()
             while True:
@@ -27,25 +32,37 @@ class Sudoku:
     def check_no_options(self):
         empty_positions = self.grid == 0
         positions_without_options = np.ndarray((self.total_size, self.total_size), dtype=bool)
-        for position in np.ndindex(self.possible_numbers.shape[:2]):
+        for position in np.ndindex(self.get_allowed_numbers().shape[:2]):
             positions_without_options[position] = not any(self.possible_numbers[(position)])
         empty_without_options = np.logical_and(empty_positions, positions_without_options)
         return np.any(empty_without_options)
 
     def rollback_last_random(self):
         for random_indicator in reversed(self.number_selection_random):
-            self.number_selection_memory.pop()
+            position = self.number_selection_memory.pop()
             self.number_selection_random.pop()
+            previous_value = self.grid[position]
+            self.grid[position] = 0
             if random_indicator:
+                self.rollbacked_filter[position, previous_value] = False
+                self.reset_dependent_rollbacks(position)
                 break
+
+    def reset_dependent_rollbacks(self, max_position):
+        positions = np.ndindex(self.grid.shape)
+        positions_filtered = []
+        for position in reversed(positions):
+            if position == max_position:
+                break
+            positions_filtered.append(position)
 
     def randomly_fill_next_position(self):
         next_position = self.get_next_free_position()
         if not next_position == -1:
             options = np.arange(1, self.total_size+1)
-            available_mask = self.possible_numbers[next_position]
-            available_options = options[available_mask]
-            choice = np.random.choice(available_options)
+            allowed_mask = self.get_allowed_numbers()[next_position]
+            allowed_options = options[allowed_mask]
+            choice = np.random.choice(allowed_options)
             self.grid[next_position] = choice
             self.number_selection_memory.append(next_position)
             self.number_selection_random.append(True)
@@ -58,13 +75,13 @@ class Sudoku:
         return -1
 
     def fill_single_choice(self, position):
-        single_option_value = np.where(self.possible_numbers[position])[0] + 1
+        single_option_value = np.where(self.get_allowed_numbers()[position])[0] + 1
         self.fill_position(position, single_option_value)
         self.update_options_single_number(position, single_option_value)
 
     def check_next_single_option_position(self):
-        for position in np.ndindex(self.possible_numbers.shape[:2]):
-            if sum(self.possible_numbers[position]) == 1 and self.grid[position] == 0:
+        for position in np.ndindex(self.get_allowed_numbers().shape[:2]):
+            if sum(self.get_allowed_numbers()[position]) == 1 and self.grid[position] == 0:
                 return position
         return -1
 
@@ -144,6 +161,6 @@ class Sudoku:
     #     result = (number in cell)
     #     return not result
 #
-x = Sudoku(size=4)
+x = Sudoku(size=9)
 x.generate_grid()
 print(x.grid)
