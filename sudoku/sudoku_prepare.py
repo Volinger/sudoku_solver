a11e411472fc477b8dd6d086662655ac1ad25624
Volinger <tisovskyerik@gmail.com>
2023-02-27 16:11:29 +0100

    documentation updated for generator, split random selection process method

--- sudoku/sudoku_prepare.py
+++ sudoku/sudoku_prepare.py
@@ -24,10 +24,21 @@ class SudokuPreparer:
         self.available_for_removal.fill(True)
 
     def prepare(self, difficulty):
+        """
+        Prepare solved sudoku for solving by removing amount of numbers from grid based on difficulty.
+        :param difficulty:
+        :return:
+        """
         self.remove_numbers(difficulty)
         return self.sudoku.grid
 
     def remove_numbers(self, difficulty: Difficulty):
+        """
+        Remove numbers in sudoku grid, until either required number of removals is reached, or no more numbers can be
+        removed without making sudoku unsolvable.
+        :param difficulty:
+        :return:
+        """
         for x in range(difficulty):
             removed = self.remove_random_number()
             if not removed:
@@ -35,25 +46,41 @@ class SudokuPreparer:
                 break
 
     def remove_random_number(self):
+        """
+        Select random position in sudoku and remove number from it, as long as this allows sudoku to be still solvable.
+        If not, rollback removal and attempt to remove other number randomly. Return true if number was successfully
+        removed. In case that all the options were exhausted and still no number was removed, return False.
+        case that all options are
+        exhausted,
+        :return:
+        """
         while True:
-            nonzero_indices = np.nonzero(self.sudoku.grid)
-            positions = np.array(list(zip(nonzero_indices[0], nonzero_indices[1])))
-            ndx = np.random.choice(len(positions))
-            position = tuple(positions[ndx])
-            previous_value = self.sudoku.grid[position]
-            self.sudoku.grid[position] = 0
+            random_position = self.select_random_filled_position()
+            previous_value = self.sudoku.grid[random_position]
+            self.sudoku.grid[random_position] = 0
             solvable = self.check_solvable()
-            self.available_for_removal[position] = False
+            self.available_for_removal[random_position] = False
             if solvable:
                 return True
             elif self.available_for_removal.any():
-                self.sudoku.grid[position] = previous_value
+                self.sudoku.grid[random_position] = previous_value
                 continue
             else:
-                self.sudoku.grid[position] = previous_value
+                self.sudoku.grid[random_position] = previous_value
                 return False
 
+    def select_random_filled_position(self):
+        nonzero_indices = np.nonzero(self.sudoku.grid)
+        nonzero_positions = np.array(list(zip(nonzero_indices[0], nonzero_indices[1])))
+        random_index = np.random.choice(len(nonzero_positions))
+        random_position = tuple(nonzero_positions[random_index])
+        return random_position
+
     def check_solvable(self):
+        """
+        Attempt to solve sudoku. Returns True if it is possible to solve it based on implemented solving steps.
+        :return:
+        """
         self.sudoku.reset_available_options()
         original_grid = self.sudoku.grid.copy()
         while True:
--- tests/test_sudoku_prepare.py
+++ tests/test_sudoku_prepare.py
@@ -8,12 +8,6 @@ import sudoku.sudoku_prepare as sudoku_prepare
 def sudoku_preparer():
     return sudoku_prepare.SudokuPreparer(size=4, seed=50)
 
-# @pytest.fixture()
-# def completed_sudoku():
-#     return [[1, 2, 4, 3]
-#             [4, 3, 2, 1]
-#             [2, 1, 3, 4]
-#             [3, 4, 1, 2]]
 
 class TestSudokuPreparer:
 
@@ -24,11 +18,11 @@ class TestSudokuPreparer:
     def test_remove_random_number(self, sudoku_preparer):
         assert sudoku_preparer.remove_random_number()
 
-    def test_check_solvable__True(self, sudoku_preparer):
+    def test_check_solvable__true(self, sudoku_preparer):
         sudoku_preparer.sudoku.grid[0, 1] = 0
         assert sudoku_preparer.check_solvable()
 
-    def test_check_solvable__False(self, sudoku_preparer):
+    def test_check_solvable__false(self, sudoku_preparer):
         sudoku_preparer.sudoku.grid = np.zeros((4, 4))
         sudoku_preparer.sudoku.possible_numbers.fill(True)
         sudoku_preparer.sudoku.rollbacked_filter.fill(True)
